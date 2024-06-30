62d5f38b90a47d61163a3cfdba6e549ee118a437
DanielBorbafs <dev.danielborba@gmail.com>
2024-06-30 12:35:05 -0300

    Limpeza de repositório

--- FOUNTAIN/script.js
+++
@@ -1,9 +0,0 @@
-const AbrirMenu = () => document.body.classList.toggle("open");
-
-function removerClasse() {
-    document.body.classList.remove("open")
-}
-
-var botaoRemover = document.getElementById("remove");
-
-botaoRemover.addEventListener("click", removerClasse)
